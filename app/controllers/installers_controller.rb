6dd2b9d66dbf57a6befeae3496e95b4ba8a943c9
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-08-30 11:14:34 -0400

    Create associations between models. Add route for installers#index with controller and view

---
+++ app/controllers/installers_controller.rb
@@ -0,0 +1,4 @@
+class InstallersController < ApplicationController
+  def index
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates :territory, presence: true
+  belongs_to :territory
+  has_many :installers, through: :territory
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
