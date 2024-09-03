f98dc30663f17424b84f70f2ad124130c54a6fe8
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-09-03 11:46:22 -0400

    Add associations to User & transactions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :territory, presence: true
   belongs_to :territory
   has_many :installers, through: :territory
+  has_many :transactions, through: :installers
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
