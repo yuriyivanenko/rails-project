2ba2b24753f2d8735955175cb4eaabf8e8b7e48d
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-09-04 09:45:52 -0400

    Finish new transaction form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   def home
     return if current_user
 
-    redirect_to new_user_session_path
+    @transactions = current_user.transactions
+
+    redirect_to new_user_session_path @transactions
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,11 @@ class Transaction < ApplicationRecord
   belongs_to :installer
   belongs_to :territory
   belongs_to :project
+
+  validates :installer_id, presence: true
+  validates :territory_id, presence: true
+  validates :description, presence: true
+  validates :amount, presence: true
+  validates :project_id, presence: true
+  validates :date, presence: true
 end
