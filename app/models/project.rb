36afbf375e18d6b1c2d9d6a9ad3d07eccb2c707e
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-09-05 09:49:06 -0400

    Add total amounts for projects index page

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ApplicationRecord
   has_many :transactions
 
   validates :project_number, presence: true
-  validates :project_manager, presence: true
+  validates :project_manager, presence: true, uniqueness: true
 end
